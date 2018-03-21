bda977ef71fac3cae8c20a0c6fe7cd7f2d9efc2c
Oscar Triano dotoscat <user@example.com>
2018-03-21 21:32:32 +0100

    Add a third test

--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
 import unittest
+from functools import partial
+from operator import add
 import funstream
 
 def sum2(a):
@@ -18,3 +20,18 @@ class Test(unittest.TestCase):
         s = funstream.Stream()
         value = 2 | s[lambda n: n + 2] | s[lambda n: n*3]
         self.assertEqual(value, 12, "With lambda no equal to 12!")
+
+    def test3_chain(self):
+        class File:
+            def __init__(self):
+                value = 0
+
+            def write(self, value):
+                self.value = value
+        f = File()
+        s = funstream.Stream()
+
+        # value = 2 | s[str] & s[f.write] | s[partial(add, '2')]
+        value = 2 | s[str] & s[f.write]
+        self.assertEqual(value, "22", "2 and 2 not 22!")
+        self.assertEqual(f.value, "2", "2 is not written by dummy file")
