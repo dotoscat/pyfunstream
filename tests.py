25e55d9dfe4ea53ff00cd5bcc5e9cf3eb329fb5f
Oscar Triano dotoscat <user@example.com>
2018-03-21 17:46:54 +0100

    Write test for lambda

--- tests.py
+++ tests.py
@@ -13,3 +13,8 @@ class Test(unittest.TestCase):
         value = 2 | s[sum2] | s[mul3]
         self.assertEqual(value, 12, "Not equal to 12!")
         print(value)
+
+    def test2_lambda(self):
+        s = funstream.Stream()
+        value = 2 | s[lambda n: n + 2] | s[lambda n: n*3]
+        self.assertEqual(value, 12, "With lambda no equal to 12!")
